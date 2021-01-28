7a256c869037968d5a036bd76e64a45f4d2e7240
Sergey Agadzhanov <agadzhanov@gmail.com>
2021-01-28 20:29:09 +0100

    -

--- src/frontend/src/App.vue
+++ src/frontend/src/App.vue
@@ -67,10 +67,10 @@
 
 		<div class="tags mt-3">
 			<span class="tag is-link" v-html="'{{Interaction.' + (activity ? activity.key : '') + '.segments}}'"></span>
-			<span class="tag is-link" v-html="'{{Interaction.' + (activity ? activity.key : '') + '.recommendations}}'"></span>
-			<span class="tag is-link" v-html="'{{Interaction.' + (activity ? activity.key : '') + '.experience}}'"></span>
 			<span class="tag is-link" v-html="'{{Interaction.' + (activity ? activity.key : '') + '.userGroup}}'"></span>
 			<span class="tag is-link" v-html="'{{Interaction.' + (activity ? activity.key : '') + '.attribute}}'"></span>
+			<span class="tag is-link" v-html="'{{Interaction.' + (activity ? activity.key : '') + '.recommendations}}'"></span>
+			<span class="tag is-link" v-html="'{{Interaction.' + (activity ? activity.key : '') + '.experience}}'"></span>
 		</div>
 
 	</div>
