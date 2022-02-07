d74954708d5dad41b23f40e5ea8187c9811bbed4
Sergey Agadzhanov <agadzhanov@gmail.com>
2022-02-07 13:02:41 +0100

    added domain validation for fuelApiEndpoint

--- src/classes/utils.ts
+++ src/classes/utils.ts
@@ -1,6 +1,7 @@
 import { Request } from 'express'
 import { verify } from 'jsonwebtoken'
-import axios, { AxiosPromise, AxiosRequestConfig, AxiosResponse } from 'axios'
+import axios, { AxiosRequestConfig } from 'axios'
+import { URL } from 'url'
 
 export default class Utils {
 	public static log(message: string, data: any) {
@@ -17,20 +18,26 @@ export default class Utils {
 
     public static async validateTenant(data: any){
         const token = data?.token?.fuel2token
-        const endpoint = data?.endpoints?.fuelapiRestHost + "platform/v1/tokenContext"
-
+        const endpoint = data?.endpoints?.fuelapiRestHost?.ToString()?.toLowerCase() + "platform/v1/tokenContext"
+        
         if(!token || !endpoint) return false
 
-        const request: AxiosRequestConfig = {
-            method: 'GET',
-            url: endpoint,
-            headers: {
-                "Content-Type": "application/json",
-                "Authorization": `Bearer ${token}`
-            }
-        }
-
         try {
+            const endpointUrl = new URL(endpoint)
+        
+            if(!endpointUrl.hostname.endsWith(".exacttargetapis.com") && !endpointUrl.hostname.endsWith(".marketingcloudapis.com")){
+                return false
+            }
+    
+            const request: AxiosRequestConfig = {
+                method: 'GET',
+                url: endpoint,
+                headers: {
+                    "Content-Type": "application/json",
+                    "Authorization": `Bearer ${token}`
+                }
+            }
+
             const response = await axios(request)
             return response?.data?.enterprise?.id?.toString() === process.env.MC_EID?.toString()
         }
